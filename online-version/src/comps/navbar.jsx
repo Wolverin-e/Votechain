1d41f19f36f1e381114a26291eaaf5db387a3021
Wolverin-e <patel.6@iitj.ac.in>
2020-01-29 02:54:15 +0530

    mobile device support

--- online-version/src/comps/navbar.jsx
+++ online-version/src/comps/navbar.jsx
@@ -21,6 +21,22 @@ class navbar extends Component {
     });
   }
 
+  handleToggle = () => {
+    const nav_links = document.getElementsByClassName('nav-item');
+    for(let i = 0; i<nav_links.length; i++){
+      nav_links[i].classList.toggle("shown");
+    }
+    document.getElementsByClassName("hamburger")[0].classList.toggle("is-active");
+  }
+
+  handleClose = () => {
+    const nav_links = document.getElementsByClassName('nav-item');
+    for(let i = 0; i<nav_links.length; i++){
+      nav_links[i].classList.remove("shown");
+    }
+    document.getElementsByClassName("hamburger")[0].classList.toggle("is-active");
+  }
+
   render() {
     return (
       <div>
@@ -30,17 +46,22 @@ class navbar extends Component {
             <li className="navbar-brand">
                 VOTECHAIN
             </li>
-            <li className="nav-item active">
+              <button class="hamburger hamburger--emphatic" type="button" onClick={() => this.handleToggle()}>
+                <span class="hamburger-box">
+                  <span class="hamburger-inner"></span>
+                </span>
+              </button>
+            <li className="nav-item active" onClick={() => this.handleClose()}>
               <Link id="homelink" to="/" className="nav-link">
                 <b>Home</b>
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item" onClick={() => this.handleClose()}>
               <Link to="/register" className="nav-link">
                 <b>Register</b>
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item" onClick={() => this.handleClose()}>
               <Link to="/about" className="nav-link">
                 <b>About</b>
               </Link>
--- online-version/src/index.js
+++ online-version/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import './hamburgers.css';
 
 import { Provider} from 'react-redux';
 import store from './store/store'
