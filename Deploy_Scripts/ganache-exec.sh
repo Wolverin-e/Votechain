a1546bf4b64d8f8b6c2ab08f0a4a2a9444d948bd
Wolverin-e <patel.6@iitj.ac.in>
2020-01-29 16:54:12 +0530

    added ganache-cli as a service for raspberry pi.

---
+++ Deploy_Scripts/ganache-exec.sh
@@ -0,0 +1,1 @@
+/home/casper/.nvm/versions/node/v10.18.1/bin/ganache-cli --host 0.0.0.0 --db="./chain-data/" -d --mnemonic="kiwi visit marble accident acid urge lumber type picnic have split charge"
